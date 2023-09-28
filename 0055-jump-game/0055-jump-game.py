a15108b4cd23c333ca27b8323deabf4a59b12227
Schmoeppel <tkillussecond@gmail.com>
2023-09-28 12:25:21 +0900

    Time: 389 ms (91.96%), Space: 17.5 MB (84.11%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        idx = len(nums)-1
+        jump_dis = 0
+
+        if idx == 0:
+            return True
+
+        while True:
+            idx -= 1
+            jump_dis += 1
+            if idx == 0:
+                if nums[idx] >= jump_dis:
+                    return True
+                else:
+                    return False
+            if nums[idx] >= jump_dis:
+                jump_dis = 0
+
+
+        # move left
+        # decrease idx
+        # increase jump_dis
+        # if element <= jump_dis
+        #    jump_dis = 0
+        # if idx == 0:
+            # if element <= jump_dis
+            #    return True
+            # else return False
+
+        
+
+        
