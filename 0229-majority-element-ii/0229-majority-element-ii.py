9d4112a7aa402db22d77505c6c6295dbcf236411
Schmoeppel <tkillussecond@gmail.com>
2023-10-05 09:57:53 +0200

    Time: 116 ms (32.65%), Space: 17.7 MB (84.36%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,11 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        unique_nums = set(nums)
+
         thresh = len(nums)/3
 
         output = []
         
-        for num in unique_nums:
+        for num in set(nums):
             if nums.count(num) > thresh:
                 output.append(num)
 
